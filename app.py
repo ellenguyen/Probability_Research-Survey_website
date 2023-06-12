f325e5ba798eb6f99784f9f28d5c7c6e36bee62a
bryanli200282 <bryanli200282@gmail.com>
2023-06-12 14:23:50 -0400

    fix typo templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request 
+from flask import Flask, render_template ,request 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hell():
-    return "hello"
+def hello():
+    return render_template("index.html")
 
 
 
