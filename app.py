b598aea8c4b951b7fdb7a56c0f80c95cc3bf4d79
bryanli200282 <bryanli200282@gmail.com>
2023-06-13 15:22:26 -0400

    setting up the home page and layout to ask about name, ID, University

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template ,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    name = request.args.get("name","world")
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("question.html", name=request.form.get("name", "world"))
 
-    return render_template("index.html", name=name)
+    
 
 
 
