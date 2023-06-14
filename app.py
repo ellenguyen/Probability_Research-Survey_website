f0e1d2e4706523a028f7974bf2355937a01ce95e
bryanli200282 <bryanli200282@gmail.com>
2023-06-14 11:42:57 -0400

    added a succes file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
-        return render_template("question.html", name=request.form.get("name", "world"))
+        return render_template("success.html")
 
     
 
