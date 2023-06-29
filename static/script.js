7ab402fccd0f9e69f27e659a31569337bbd06f70
Elle Nguyen <tuo84526@temple.edu>
2023-06-29 00:53:48 -0400

    testing css styling

--- static/script.js
+++ static/script.js
@@ -5,6 +5,6 @@ box1.addEventListener("click", function() {
   var amount = parseInt(box1.innerHTML.substring(1));
   if (amount > 25) {
     amount -= 10;
-    box1.innerHTML = "$" + amount;
+    box1.innerHTML = "$ " + amount;
   }
 });
