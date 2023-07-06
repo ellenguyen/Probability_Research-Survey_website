db4a6e9f441353de7eab5aea1d71ba8c52feeab2
Elle Nguyen <tuo84526@temple.edu>
2023-07-06 10:27:11 -0400

    input table

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # import libraries to redirect to different page layouts
 from flask import Flask, render_template, request, redirect, url_for
 
-#install pip install Flask psycopg2-binary
+# install pip install Flask psycopg2-binary
 import psycopg2
-#install pip install pyyaml
+
+# install pip install pyyaml
 import yaml
 
 # library to randomly redirect users
@@ -17,15 +18,14 @@ classes = [
         "STAT 1001",
         "MATH 1001",
     ]
-#connect DATABSE
-db = yaml.safe_load(open('db.yaml'))
-conn = psycopg2.connect(
-        host= db['postgres_host'],
-        database= db["postgres_db"],
-        user= db["postgres_user"],
-        password= db['postgres_password'])
 
-
+# connect DATABASE
+# db = yaml.safe_load(open('db.yaml'))
+# conn = psycopg2.connect(
+#         host= db['postgres_host'],
+#         database= db["postgres_db"],
+#         user= db["postgres_user"],
+#         password= db['postgres_password'])
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -33,21 +33,21 @@ def index():
         return render_template("index.html",classes=classes)
 
     elif request.method == "POST":
-        #asking the request from the user 
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        student_id = request.form['student_id']
-        class_taking = request.form['class']
-        instructor = request.form['instructor']
-        major = request.form['major']
-        university_year = request.form['university_year']
-        statistics = request.form['statistics']
+        # asking the request from the user 
+        # first_name = request.form['first_name']
+        # last_name = request.form['last_name']
+        # student_id = request.form['student_id']
+        # class_taking = request.form['class']
+        # instructor = request.form['instructor']
+        # major = request.form['major']
+        # university_year = request.form['university_year']
+        # statistics = request.form['statistics']
 
-        #creating a cursor to use DATABASE operations
-        cur = conn.cursor()
-        #insert the data into the database
+        # creating a cursor to use DATABASE operations
+        # cur = conn.cursor()
+        # insert the data into the database
 
-        cur.execute('''INSERT INTO ['table_name'] (first_name, last_name,student_id,class_taking, instructor, major, university_year, statistics) VALUES(%s, %s)''', (first_name, last_name,student_id,class_taking, instructor, major, university_year, statistics))
+        # cur.execute('''INSERT INTO ['table_name'] (first_name, last_name,student_id,class_taking, instructor, major, university_year, statistics) VALUES(%s, %s)''', (first_name, last_name,student_id,class_taking, instructor, major, university_year, statistics))
         # conn.commit()
         # cur.close()
         # conn.close()
@@ -56,7 +56,7 @@ def index():
         # generate a random number between 0 and 1
         random_number = random.random()
 
-        if random_number < 0.9:
+        if random_number < 0.5:
             # run plain_text
             return render_template("/plain_text/plain_text.html",classes=classes)
         else:
