24990e96a7d5bc8a6bf982ac05d1ebf0c0be1753
bryanli200282 <bryanli200282@gmail.com>
2023-07-09 14:36:35 -0400

    fix the plan_text file and added the welcome file

--- static/script.js
+++ static/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   const cashAmounts = [30, 50, 70, 90, 100, 120, 140, 160, 180, 200];
-  const cashAmountsRound2 = [120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140];
+  //const cashAmountsRound2 = [120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140];
+  //creating the user choice for CASH or LOTTERY
+  const choices = {
+    "Cash": false,
+    "Lottery" : false
+  }
+
+  for (let key in choices) {
+    let label = document.createElement("label");
+    label.innerText = key;
+  }
 
   const round1TableBody = document.getElementById("round1-table-body");
 
@@ -8,50 +18,58 @@ document.addEventListener("DOMContentLoaded", function() {
   cashAmounts.forEach(function(cashAmount) {
     const row = document.createElement("tr");
 
+
+    // adding the Letorry text and cell on to the table 
     const lotteryCell = document.createElement("td");
-    const lotteryText = document.createTextNode("75% of $200 25% of $30");
+    const lotteryText = document.createTextNode("Loterry");
     lotteryCell.appendChild(lotteryText);
     row.appendChild(lotteryCell);
 
+    //creating the cash amount element
     const cashAmountCell = document.createElement("td");
     cashAmountCell.textContent = "$" + cashAmount;
     row.appendChild(cashAmountCell);
 
     const userInputCell = document.createElement("td");
-    const userInputInput = document.createElement("input");
-    userInputInput.type = "text";
-    userInputInput.required = true; // Set input as required
-    userInputCell.appendChild(userInputInput);
+    //const choice = document.createElement("input");
+    
+
+    // choice.type = "radio";
+    // choice.type = "radio";
+    // row.appendChild(choice);
+    // round1TableBody.appendChild(row)
+    
+    //create the label element 
+
+    // let label = document.createElement("label");
+    // label.innerText = "Lottery";
+    // userInputCell.appendChild(label)
+    // userInputInput.type = "radio";
+    // //userInputInput.required = true; // Set input as required
+    // label.appendChild(userInputInput);
+    // row.appendChild(userInputCell);
+
+  
+
+    for (let key in choices) {
+      let label = document.createElement("label");
+      label.innerText = key;
+      const userInputInput = document.createElement("input");
+      
+      userInputInput.name = "choice"
+      userInputInput.type = "radio";
+      label.appendChild(userInputInput);
+      userInputCell.appendChild(label);
+   }
     row.appendChild(userInputCell);
 
     round1TableBody.appendChild(row);
   });
 
-  // Style the lotteryCell in the first table
+  //Style the lotteryCell in the first table
   const lotteryCells = document.querySelectorAll("#round1-table-body td:first-child");
   lotteryCells.forEach(function(cell) {
     cell.classList.add("lottery-cell");
-  });
-
-  // Validate user input in the first table
-  round1TableBody.addEventListener("input", function(event) {
-    const userInput = event.target.value.trim(); // Trim whitespace from the input
-    const userInputCell = event.target.parentNode;
-    const cashAmountCell = userInputCell.previousElementSibling;
-
-    if (userInput !== "L" && userInput !== "C" || userInput === "") {
-      userInputCell.classList.add("invalid-input");
-    } else {
-      userInputCell.classList.remove("invalid-input");
-    }
-
-    const allInputsFilled = Array.from(round1TableBody.querySelectorAll("input")).every(function(input) {
-      return input.value.trim() !== "";
-    });
-
-    if (allInputsFilled) {
-      createRound2Table();
-    }
   });
 
   function createRound2Table() {
@@ -80,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function() {
       const userInputCell = document.createElement("td");
       const userInputInput = document.createElement("input");
       userInputInput.type = "text";
-      userInputInput.required = true; // Set input as required
+      userInputInput.required = true; // Set input as requx ired
       userInputCell.appendChild(userInputInput);
       row.appendChild(userInputCell);
 
@@ -92,28 +110,30 @@ document.addEventListener("DOMContentLoaded", function() {
     // Append the round 2 table to the container element
     container.appendChild(round2Table);
 
-    // Style the lotteryCell in the second table
-    const lotteryCellsRound2 = round2Table.querySelectorAll("td:first-child");
-    lotteryCellsRound2.forEach(function(cell) {
-      cell.classList.add("lottery-cell");
-    });
+    // //Style the lotteryCell in the second table
+    // const lotteryCellsRound2 = round2Table.querySelectorAll("td:first-child");
+    // lotteryCellsRound2.forEach(function(cell) {
+    //   cell.classList.add("lottery-cell");
+    // });
 
-    // Validate user input in the second table
-    round2TableBody.addEventListener("input", function(event) {
-      const userInput = event.target.value.trim(); // Trim whitespace from the input
-      const userInputCell = event.target.parentNode;
-      const cashAmountCell = userInputCell.previousElementSibling;
+    // //Validate user input in the second table
+    // round2TableBody.addEventListener("input", function(event) {
+    //   const userInput = event.target.value.trim(); // Trim whitespace from the input
+    //   const userInputCell = event.target.parentNode;
+    //   const cashAmountCell = userInputCell.previousElementSibling;
 
-      if (userInput !== "L" && userInput !== "C" || userInput === "") {
-        userInputCell.classList.add("invalid-input");
-      } else {
-        userInputCell.classList.remove("invalid-input");
-      }
+    //   if (userInput !== "L" && userInput !== "C" || userInput === "") {
+    //     userInputCell.classList.add("invalid-input");
+    //   } else {
+    //     userInputCell.classList.remove("invalid-input");
+    //   }
 
-      const allInputsFilled = Array.from(round2TableBody.querySelectorAll("input")).every(function(input) {
-        return input.value.trim() !== "";
-      });
+    //   const allInputsFilled = Array.from(round2TableBody.querySelectorAll("input")).every(function(input) {
+    //     return input.value.trim() !== "";
+    //   });
       
-    });
+    // });
   }
+
+  
 });
