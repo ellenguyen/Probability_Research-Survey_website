8eebbb4593d9ef7b9149587871a8269ca41aeb03
bryanli200282 <bryanli200282@gmail.com>
2023-07-12 09:43:32 -0400

    minor changes on the js file

--- static/script.js
+++ static/script.js
@@ -1,26 +1,20 @@
-function initializeLotteryTable() {
-  const lotteries = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-  const round1TableBody = document.getElementById("round1-table-body");
-  const choices = {
-    "Cash": false,
-    "Lottery": false
-  };
-
-
-  lotteries.forEach(function(cashAmount, i) {
+function createLotteryRows(cashAmounts, choices, tableBodyId) {
+  const tableBody = document.getElementById(tableBodyId);
+  
+  cashAmounts.forEach(function(cashAmount, i) {
     const row = document.createElement("tr");
-
+  
     const lotteryCell = document.createElement("td");
     const lotteryText = document.createTextNode("Lottery");
     lotteryCell.appendChild(lotteryText);
     row.appendChild(lotteryCell);
-
+  
     const cashAmountCell = document.createElement("td");
     cashAmountCell.textContent = "$" + cashAmount;
     row.appendChild(cashAmountCell);
-
+  
     const userInputCell = document.createElement("td");
-
+  
     for (let key in choices) {
       let label = document.createElement("label");
       label.innerText = key;
@@ -30,15 +24,26 @@ function initializeLotteryTable() {
       label.appendChild(userInputInput);
       userInputCell.appendChild(label);
     }
-
+  
     row.appendChild(userInputCell);
-    round1TableBody.appendChild(row);
+    tableBody.appendChild(row);
   });
-
-  const lotteryCells = round1TableBody.querySelectorAll("td:first-child");
+  
+  const lotteryCells = tableBody.querySelectorAll("td:first-child");
   lotteryCells.forEach(function(cell) {
     cell.classList.add("lottery-cell");
   });
+}
+
+function initializeLotteryTable() {
+  const lotteries = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+  const choices = {
+    "Cash": false,
+    "Lottery": false
+  };
+  const tableBodyId = "round1-table-body";
+
+  createLotteryRows(lotteries, choices, tableBodyId);
 }
 
 document.addEventListener("DOMContentLoaded", initializeLotteryTable);
