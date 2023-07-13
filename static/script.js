e118f3053e1378073347aadbcd204ed9cfe9b4c3
bryanli200282 <bryanli200282@gmail.com>
2023-07-13 11:31:50 -0400

    function change in javascript

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// function for top create the loterry rows
 function createLotteryRows(cashAmounts, choices, tableBodyId) {
   const tableBody = document.getElementById(tableBodyId);
   
@@ -18,9 +19,14 @@ function createLotteryRows(cashAmounts, choices, tableBodyId) {
     for (let key in choices) {
       let label = document.createElement("label");
       label.innerText = key;
+      if (i < 1){
+          var f = document.createElement("form");
+          f.setAttribute('method',"post");
+      }
       const userInputInput = document.createElement("input");
       userInputInput.name = "choice" + i;
       userInputInput.type = "radio";
+      //f.appendChild(userInputInput)
       label.appendChild(userInputInput);
       userInputCell.appendChild(label);
     }
@@ -35,15 +41,29 @@ function createLotteryRows(cashAmounts, choices, tableBodyId) {
   });
 }
 
+
+
+//Function to generate a range of lotteries with a specified step size
+function generateLotteryRange(start, end, step) {
+  const range = [];
+  for (let i = start; i <= end; i += step) {
+    range.push(i);
+  }
+  return range;
+}
+
+//first call
 function initializeLotteryTable() {
   const lotteries = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+  const lotteries1 = generateLotteryRange(5, 50, 5);
   const choices = {
     "Cash": false,
     "Lottery": false
   };
   const tableBodyId = "round1-table-body";
 
-  createLotteryRows(lotteries, choices, tableBodyId);
+  createLotteryRows(lotteries1, choices, tableBodyId);
 }
 
 document.addEventListener("DOMContentLoaded", initializeLotteryTable);
+
