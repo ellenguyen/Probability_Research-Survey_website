abc9c46f40e293741bab0c25fbb440e89c2f2c36
bryanli200282 <bryanli200282@gmail.com>
2023-07-15 19:10:25 -0400

    changes the plain text file for the table

--- static/script.js
+++ static/script.js
@@ -1,47 +1,62 @@
+// Creating the rows
 function createLotteryRows(cashAmounts, choices, tableBodyId) {
   const tableBody = document.getElementById(tableBodyId);
-  
-  cashAmounts.forEach(function(cashAmount, i) {
-    const row = document.createElement("tr");
-  
-    const lotteryCell = document.createElement("td");
-    const lotteryText = document.createTextNode("Lottery");
-    lotteryCell.appendChild(lotteryText);
-    row.appendChild(lotteryCell);
-  
-    const cashAmountCell = document.createElement("td");
-    cashAmountCell.textContent = "$" + cashAmount;
-    row.appendChild(cashAmountCell);
-  
-    const userInputCell = document.createElement("td");
-  
-    for (let key in choices) {
-      let label = document.createElement("label");
-      label.innerText = key;
-      const userInputInput = document.createElement("input");
-      userInputInput.name = "choice" + i;
-      userInputInput.type = "radio";
-      label.appendChild(userInputInput);
-      userInputCell.appendChild(label);
-    }
-  
-    row.appendChild(userInputCell);
-    tableBody.appendChild(row);
-  });
-  
-  const lotteryCells = tableBody.querySelectorAll("td:first-child");
-  lotteryCells.forEach(function(cell) {
-    cell.classList.add("lottery-cell");
+
+  cashAmounts.forEach((cashAmount, index) => {
+      const divTr = document.createElement("div");
+      divTr.className = "tr";
+
+      const divLabel = document.createElement("div");
+      divLabel.className = "td";
+      divLabel.textContent = "Lottery";
+      divTr.appendChild(divLabel);
+
+      const divAmount = document.createElement("div");
+      divAmount.className = "td";
+      divAmount.textContent = "$" + cashAmount;
+      divTr.appendChild(divAmount);
+
+      const divChoices = document.createElement("div");
+      divChoices.className = "td";
+
+      for (let choice in choices) {
+          const label = document.createElement("label");
+          const radioInput = document.createElement("input");
+          radioInput.type = "radio";
+          radioInput.name = "choice" + index;
+          radioInput.value = choice;
+          radioInput.required = true; // Added required attribute
+
+          const span = document.createElement("span");
+          span.textContent = choice;
+
+          label.appendChild(radioInput);
+          label.appendChild(span);
+          divChoices.appendChild(label);
+      }
+
+      divTr.appendChild(divChoices);
+      tableBody.appendChild(divTr);
   });
 }
 
+// Function to generate a range of lotteries with a specified step size
+function generateLotteryRange(start, end, step) {
+  const range = [];
+  for (let i = start; i <= end; i += step) {
+      range.push(i);
+  }
+  return range;
+}
+
+// First call
 function initializeLotteryTable() {
-  const lotteries = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+  const lotteries = generateLotteryRange(5, 50, 5);
   const choices = {
-    "Cash": false,
-    "Lottery": false
+      "Cash": "Cash",
+      "Lottery": "Lottery"
   };
-  const tableBodyId = "round1-table-body";
+  const tableBodyId = "tbody";
 
   createLotteryRows(lotteries, choices, tableBodyId);
 }
