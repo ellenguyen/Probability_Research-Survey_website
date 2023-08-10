a9fd655b6700bf52d24c35b9504a40442b44684a
bryanli200282 <bryanli200282@gmail.com>
2023-08-09 20:27:23 -0400

    random

--- app.py
+++ app.py
@@ -108,26 +108,28 @@ def instruction():
 LOTTERIES = 25
 available_lotteries = list(range(1, MAX_LOTTERY+1))
 
+
 @app.route('/lottery', methods=['GET'])
 @app.route('/lottery/', methods=['GET'])
 @app.route('/lottery/<lottery_num>', methods=['GET', 'POST'])
 def lottery(lottery_num=1):
     #Generate a list of all available lotteries
+    global available_lotteries
     global LOTTERIES
     
-    #print(available_lotteries)
+    print(available_lotteries)
 
     #print(MAX_LOTTERY)
     #index so i can remove
     if(len(available_lotteries) != 0):
-        rand = random.randint(0, LOTTERIES-1)
+        rand = random.randrange(0, LOTTERIES)
         lottery_num = available_lotteries.pop(rand)
-        #print("randon", rand)
+        print("randon", rand)
         LOTTERIES -= 1
 
     
     
-    #print("lottery_num", lottery_num)
+    print("lottery_num", lottery_num)
     lottery_num = str(lottery_num)
 
     session['lottery_num'] = lottery_num
